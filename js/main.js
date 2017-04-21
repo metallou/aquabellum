98ebc0dbe991999cc00896030e3042a2ea767f42
GLoSL <felix.og@laposte.net>
2017-04-21 18:45:38 +0200

    added select function for rotation button and captured ship name in variable

--- js/main.js
+++ js/main.js
@@ -2,44 +2,64 @@
 
 const launchGame = {};
 
-const shipButtonRadioSelect = function() {
-	let shipButtons = document.getElementsByClassName('ship-button');
-
+const shipButtonSelect = function(ship)
+{
+	const shipButtons = document.getElementsByClassName('ship-button');
   for (let shipButton of shipButtons) {
-    shipButton.addEventListener('click', function(event) {
-			for (shipButton of shipButtons) {
+    shipButton.addEventListener('click', function(e)
+		{
+			for (let shipButton of shipButtons) {
 				shipButton.classList.remove('radio-selected');
 			}
-			event.target.classList.add('radio-selected');
+			e.target.classList.add('radio-selected');
+			ship.name = e.target.name;
+
+			//Enlever bateau si placé
+			//recalculer position unavailable
     });
   }
 }
+const rotationButtonSelect = function(ship)
+{
+	document.getElementById("rotation").addEventListener('click', function(e)
+		{
+			console.info(ship);
+			ship.rotation = !ship.rotation;
 
-const placingPhase = function() {
-	shipButtonRadioSelect();
+			//recalculer position unavailable
+    });
+}
 
+const placingPhase = function()
+{
+	let placingShip = {
+		name: "",
+		rotation: false,
+		row: 0,
+		column: 0
+	};
+
+	//Repérage blocs impossibles
+
+	shipButtonSelect(placingShip);
+	rotationButtonSelect(placingShip);
+	//addEventListener blocks
 
 }
 
-launchGame.playerVsPlayer = function() {
-	let gridP1 = new Grid("self"),
-	    gridP2 = new Grid("other");
+launchGame.playerVsPlayer = function()
+{
+	let gridP1 = new Grid("self");
+	let gridP2 = new Grid("other");
 
   placingPhase();
 
-
-	console.log(gridP1.visualise());
+	gridP1.visualise();
 	console.log(gridP2);
 }
 
-
-
 const mainReady = function()
 {
-
-	document.getElementById('PLAY')
-	        .addEventListener('click', launchGame.playerVsPlayer);
-
+	document.getElementById('PLAY').addEventListener('click', launchGame.playerVsPlayer);
 }
-
 document.addEventListener("DOMContentLoaded", mainReady);
