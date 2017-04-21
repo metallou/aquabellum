69879b44a865282dd3eade9359a031ff813175a8
Metallou <kevincastejon13@gmail.com>
2017-04-21 16:31:52 +0200

    few sounds added

--- js/events.js
+++ js/events.js
@@ -40,6 +40,7 @@ const readyEvents = function()
   for(let i=0; i<infoButtons.length; i++) {
     infoButtons.item(i).addEventListener("click", function(e)
         {
+          playSound("bubble");
           const id = e.target.parentNode.getElementsByTagName("span")[0].id;
           document.getElementById("info"+id).style["display"] = "block";
         });
@@ -48,6 +49,7 @@ const readyEvents = function()
   for(let i=0; i<infoPages.length; i++) {
     infoPages.item(i).addEventListener("click", function(e)
         {
+          playSound("pop");
           let elem = e.target;
           while(!elem.classList.contains("info")) elem = elem.parentNode;
           elem.style["display"] = "none";
