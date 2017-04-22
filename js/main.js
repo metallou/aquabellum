d8f87a6a47476a14ac44c3e578f5b736f92cc087
GLoSL <felix.og@laposte.net>
2017-04-22 21:02:31 +0200

    add .impossible class on block if ship can't be placed on it

--- js/main.js
+++ js/main.js
@@ -2,13 +2,29 @@
 
 const launchGame = {};
 
-const shipButtonSelect = function(ship)
-{
+const checkUnavalaibleCells = function() {
+	let htmlPlayer1Grid = document.getElementById('grid_p1'),
+	    htmlPlayer1Rows = htmlPlayer1Grid.getElementsByClassName('row');
+
+	for (let htmlPlayer1Row of htmlPlayer1Rows) {
+    let cells = htmlPlayer1Row.getElementsByClassName('cell');
+    for (let i = 0; i < cells.length; i++) {
+			if (cells[i].classList.contains('ship')) {
+        cells[i].classList.add('impossible');
+			}
+		}
+
+  }
+
+}
+
+const shipButtonSelect = function(ship) {
 	const shipButtons = document.getElementsByClassName('ship-button');
   for (let shipButton of shipButtons) {
     shipButton.addEventListener('click', function(e)
 		{
-			for (let shipButton of shipButtons) {
+			checkUnavalaibleCells();
+			for (shipButton of shipButtons) {
 				shipButton.classList.remove('radio-selected');
 			}
 			e.target.classList.add('radio-selected');
@@ -19,8 +35,8 @@ const shipButtonSelect = function(ship)
     });
   }
 }
-const rotationButtonSelect = function(ship)
-{
+
+const rotationButtonSelect = function(ship) {
 	document.getElementById("rotation").addEventListener('click', function(e)
 		{
 			console.info(ship);
@@ -30,16 +46,29 @@ const rotationButtonSelect = function(ship)
     });
 }
 
-const placingPhase = function()
-{
+const placingPhase = function() {
+
 	let placingShip = {
-		name: "",
-		rotation: false,
-		row: 0,
-		column: 0
-	};
+				name: "",
+				rotation: false,
+				row: 0,
+				column: 0
+			};
 
 	//Repérage blocs impossibles
+
+
+
+
+	// for (let htmlPlayer1Cell of htmlPlayer1Cells) {
+	//   if (!htmlPlayer1Cell.canPlaceShipAt()) {
+	// 		htmlPlayer1Cell.classList.add('impossible');
+	// 		console.log(htmlPlayer1Cell + "has a ship");
+	// 	}
+
+		// htmlPlayer1Cell hasShip() ? htmlPlayer1Cell.classList.add('impossible') : console.log(htmlPlayer1Cell + "oké");
+	// }
+
 
 	shipButtonSelect(placingShip);
 	rotationButtonSelect(placingShip);
@@ -47,19 +76,18 @@ const placingPhase = function()
 
 }
 
-launchGame.playerVsPlayer = function()
-{
-	let gridP1 = new Grid("self");
-	let gridP2 = new Grid("other");
+launchGame.solo = function() {
+	let gridP1 = new Grid("self"),
+	    bot = new Grid("BOTeasy");
 
   placingPhase();
 
 	gridP1.visualise();
-	console.log(gridP2);
+	console.log(gridP1);
 }
 
-const mainReady = function()
-{
-	document.getElementById('PLAY').addEventListener('click', launchGame.playerVsPlayer);
+const mainReady = function() {
+	document.getElementById('PLAY').addEventListener('click', launchGame.solo);
 }
+
 document.addEventListener("DOMContentLoaded", mainReady);
